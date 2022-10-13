a104b98525d816dfdd3572bca5dfd45be8de42b3
lkaldes <lkaldes@ucsc.edu>
2022-10-13 15:00:05 -0700

    movement started

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] ghosts;
+    public GameObject pacman;
+    public Transform pellets;
+    public Transform fruit;
+}
